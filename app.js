de7b0ccab0ddc2950e1f5763965f8b4132a4517a
gnaniluca <gnani.luca@iisgalvanimi.edu.it>
2024-03-05 08:47:42 +0000

    compito finito

--- app.js
+++ app.js
@@ -1,51 +1,54 @@
 const express = require('express');
-
-const Mnaked= require('./json/naked.json');
-const Msport= require('./json/sportive.json');
-const MTutte= require('./json/Tmoto.json');
 const app = express();
+
+const Mnaked = require('./json/naked.json');
+const Msport = require('./json/sportive.json');
+const MTutte = require('./json/Tmoto.json');
+
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
 
 app.get('/', (req, res) => {
-
     res.render('home', {
-    title: 'Homepage',
-    sportive: Msport['sportive'],
-    moto: Mnaked['naked']
-
+        title: 'Homepage',
+        sportive: Msport.sportive,
+        moto: Mnaked.naked
+        
     });
 });
 
 app.get('/naked', (req, res) => {
     res.render('moto', {
-    title: 'LE NOSTRE MOTO SUV',
-    moto: Mnaked['naked'],
-    variabile: "naked"
+        title: 'LE NOSTRE MOTO SUV',
+        moto: Mnaked.naked,
+        variabile: "naked"
     });
 });
 
 app.get('/sportive', (req, res) => {
-
     res.render('moto', {
-    title: 'LE NOSTRE MOTO SPORTIVE',
-    moto: Msport['sportive'],
-    variabile: "sportive"
-    
+        title: 'LE NOSTRE MOTO SPORTIVE',
+        moto: Msport.sportive,
+        variabile: "sportive"
     });
 });
 
-app.get('/profile', (req, res) => {
-    const mid = MTutte.moto.find((m) => m.id == req.query.id);
-    const variabile= req.query.page;
-    res.render('info', {
-        title: `About ${mid.nome}`, 
-        mid,
-        variabile
-    });
+app.get('/moto/:id', (req, res) => {
+    const motoId = req.params.id;
+    const moto = MTutte['tutte moto'].find((moto) => moto.id === parseInt(motoId));
+    if (moto) {
+        res.render('info', {
+            title: `About ${moto.nome}`,
+            d: moto,
+            variabile: 'Moto'
+        });
+    } else {
+        res.status(404).send('Auto non trovata');
+    }
 });
 
 
-app.listen(3000, function () {
-console.log('Example app listening on port 3000!');
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
 });
